2f754333601c5a832de00ceb2cde32c4d024a272
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-26 12:01:55 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- header -->
+    <HeaderCom/>
+
+    <!-- main -->
+    <MainCom/>
+
+    <!-- footer -->
+    <FooterCom/>
+  </div>
+</template>
+
+<script>
+import HeaderCom from './components/HeaderCom.vue';
+import MainCom from './components/MainCom.vue';
+import FooterCom from './components/FooterCom.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+    HeaderCom,
+
+    MainCom,
+
+    FooterCom,
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/MainCom.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+   
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainCom',
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
