bfafae6f1f5eb1f0bf60f5acf8125037ac0284c2
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-26 23:59:01 +0200

    Ho caricato le immagini con le card

--- src/components/TreatmentCard.vue
+++ src/components/TreatmentCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
-      <div v-for="item in cards" :key="item.image_path" >
-            <img :src="`@/img/Layer-${item.image_path}-new.jpg`" :alt="item.title">
+      <div v-for="item in cards" :key="item.image_path" class="content-card">
+            <img :src="require('@/img/layer-' + item.image_path + '-new.jpg')">
             <h3> {{item.title}} </h3>
             <div></div>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam laborum tempore, illum maxime repellendus! Quia corporis amet vitae eos!</p>
@@ -16,11 +16,30 @@ export default {
 
   props:{
       cards: Array,
+  },
+
+  methods: {
+      imgUrl(item){
+
+          const url = `@/img/layer-${item.image_path}-new.jpg`;
+
+          return url;
+
+      }
   }
 }
 </script>
 
 
 <style scoped lang="scss">
-
+    .card{
+        display: flex;
+        flex-wrap: wrap;
+        .content-card{
+            width: 25%;
+            img{
+                width: 100%;
+            }
+        }
+    }
 </style>
