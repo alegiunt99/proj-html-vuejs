c4e1bbb1a5f3c1145a488c4d7e14cb1f2ddc1961
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-27 10:51:03 +0200

    Ho creato la sezione della citazione con sfondo

--- src/components/CitSection.vue
+++ src/components/CitSection.vue
@@ -1,6 +1,12 @@
 <template>
   <section>
-   
+    <div class="container">
+      <p>"If you want to relax, watch the clouds pass by if
+        you're laying on the grass, or sit in front of creek;
+        just doing nothing and having those still moments is
+        what really rejuvenates the body..." </p>
+      <h3>FRIEDA FRIESDSON, MASSEUSE</h3>
+    </div>
   </section>
 </template>
 
@@ -12,5 +18,21 @@ export default {
 
 
 <style scoped lang="scss">
-
+  section{
+    background-image: url('@/img/Home-quota-new.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  
+  .container{
+    padding: 219px 181px;
+    text-align: center;
+    color: white;
+    p{
+      margin-bottom: 24px;
+      font-size: 24px;
+      line-height: 1.8rem;
+    }
+    
+  }
 </style>
